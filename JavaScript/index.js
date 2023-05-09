2ca5fa1f0b0db4c1147e46ee9bfc273bce65bfc5
Chirag Simkhada <chiragsimkhada@gmail.com>
2023-05-09 12:27:38 +0545

    CSS

---
+++ JavaScript/index.js
@@ -0,0 +1,55 @@
+// console.log('Hello World')
+// let userInput = prompt("Enter a text")
+// console.log(userInput)
+// let userChoice = confirm("Are You Sure?")
+// console.log(userChoice)
+// alert("Alert")
+
+
+// Data Types (String, Boolean, Numbers, Array, Objects)
+
+let str1 = "21"
+let str2 = "10"
+let str3 = "a"
+let bool1 = true
+let bool2 = false
+let num1 = 1
+let num2 = 1.05
+
+console.log(str1 * str2 + str3)
+
+console.log(typeof (str1))
+
+
+
+let obj = {
+    "name": "Chirag",
+    "age": 25,
+    "address": "Kathmandu"
+}
+console.log(obj["address"])
+
+
+// let arr1 = [1, 2, 3, "a", "b", "c"]
+// console.log(arr1)
+// arr1[0] = 5
+// console.log(arr1)
+
+
+let var5 = 10
+let var6 = "10"
+// console.log(var5 === var6)
+
+
+const userInput = prompt("Enter a number")
+
+if (userInput == 10) {
+    console.log("Valid")
+}
+
+else {
+    console.log("Invalid")
+}
+
+
+
